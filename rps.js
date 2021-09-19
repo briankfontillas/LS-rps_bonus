426e49c8a0109ae9c676a0d5307fd28ec3e4bd62
Brian Fontillas <BrianKFontillas@gmail.com>
2021-09-18 23:38:31 -0700

    now added lizard and spock. Also added consistant score system

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 const readline = require('readline-sync');
-const VALID_INPUTS = ['Rock', 'Paper', 'Scizzors'];
+const VALID_INPUTS = ['Rock', 'Paper', 'Scizzors', 'Lizard', 'Spock'];
 let myPoints = 0;
 let computerPoints = 0;
 
@@ -15,55 +15,80 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(number);
 }
 
-while (true) {
-  let computerAnswer = Math.floor(Math.random() * 3);
-  prompt('Welcome to Rock, Paper, Scizzors!');
+function displayWinner(myAnswer, computerAnswer) {
+  if ((VALID_INPUTS[myAnswer] === 'Rock' &&
+      (VALID_INPUTS[computerAnswer] === 'Scizzors' || VALID_INPUTS[computerAnswer] === 'Lizard')) ||
+      (VALID_INPUTS[myAnswer] === 'Paper' &&
+      (VALID_INPUTS[computerAnswer] === 'Rock' || VALID_INPUTS[computerAnswer] === 'Spock')) ||
+      (VALID_INPUTS[myAnswer] === 'Scizzors' &&
+      (VALID_INPUTS[computerAnswer] === 'Paper' || VALID_INPUTS[computerAnswer] === 'Lizard')) ||
+      (VALID_INPUTS[myAnswer] === 'Lizard' &&
+      (VALID_INPUTS[computerAnswer] === 'Spock' || VALID_INPUTS[computerAnswer] === 'Paper')) ||
+      (VALID_INPUTS[myAnswer] === 'Spock' &&
+      (VALID_INPUTS[computerAnswer] === 'Rock' || VALID_INPUTS[computerAnswer] === 'Scizzors'))) {
+    return true;
+  } else if ((VALID_INPUTS[myAnswer] === 'Rock' &&
+      (VALID_INPUTS[computerAnswer] === 'Spock' || VALID_INPUTS[computerAnswer] === 'Paper')) ||
+      (VALID_INPUTS[myAnswer] === 'Paper' &&
+      (VALID_INPUTS[computerAnswer] === 'Scizzors' || VALID_INPUTS[computerAnswer] === 'Lizard')) ||
+      (VALID_INPUTS[myAnswer] === 'Scizzors' &&
+      (VALID_INPUTS[computerAnswer] === 'Spock' || VALID_INPUTS[computerAnswer] === 'Rock')) ||
+      (VALID_INPUTS[myAnswer] === 'Lizard' &&
+      (VALID_INPUTS[computerAnswer] === 'Rock' || VALID_INPUTS[computerAnswer] === 'Scizzors')) ||
+      (VALID_INPUTS[myAnswer] === 'Spock' &&
+      (VALID_INPUTS[computerAnswer] === 'Lizard' || VALID_INPUTS[computerAnswer] === 'Paper'))) {
+    return false;
+  }
+}
+
+prompt('Welcome to Rock, Paper, Scizzors, Lizard, Spock (Best outta 5)!');
+
+while (myPoints < 3 || computerPoints < 3) {
   console.log('_______________________________________');
+  let opAnswer = Math.floor(Math.random() * 3);
 
   prompt('Please select from the following:');
-  console.log('1.) Rock\n2.) Paper\n3.) Scizzors\n');
-  let myAnswer = readline.question();
+  console.log('1.) Rock\n2.) Paper\n3.) Scizzors\n4.) Lizard\n5.) Spock\n');
+  let userAnswer = readline.question();
 
-  while (invalidNumber(myAnswer) || !['1', '2', '3'].includes(myAnswer)) {
-    prompt(`${myAnswer} is invalid. Please try again`);
-    myAnswer = readline.question();
+  while (invalidNumber(userAnswer) || !['1', '2', '3', '4', '5'].includes(userAnswer)) {
+    prompt(`${userAnswer} is invalid. Please try again`);
+    userAnswer = readline.question();
   }
 
-  myAnswer = Number(myAnswer) - 1;
+  userAnswer = Number(userAnswer) - 1;
 
-  prompt(`Your choice: ${VALID_INPUTS[myAnswer]}`);
-  prompt(`Opponent choice: ${VALID_INPUTS[computerAnswer]}`);
+  prompt(`Your choice: ${VALID_INPUTS[userAnswer]}`);
+  prompt(`Opponent choice: ${VALID_INPUTS[opAnswer]}`);
 
-  if ((VALID_INPUTS[myAnswer] === 'Rock' && VALID_INPUTS[computerAnswer] === 'Scizzors') ||
-      (VALID_INPUTS[myAnswer] === 'Paper' && VALID_INPUTS[computerAnswer] === 'Rock') ||
-      (VALID_INPUTS[myAnswer] === 'Scizzors' && VALID_INPUTS[computerAnswer] === 'Paper')) {
-    prompt('You are the WINNER!');
+  if (displayWinner(userAnswer, opAnswer) === true) {
     myPoints += 1;
-  } else if ((VALID_INPUTS[myAnswer] === 'Rock' && VALID_INPUTS[computerAnswer] === 'Paper') ||
-            (VALID_INPUTS[myAnswer] === 'Paper' && VALID_INPUTS[computerAnswer] === 'Scizzors') ||
-            (VALID_INPUTS[myAnswer] === 'Scizzors' && VALID_INPUTS[computerAnswer] === 'Rock')) {
-    prompt('You Lose!');
+  } else if (displayWinner(userAnswer, opAnswer) === false) {
     computerPoints += 1;
   } else {
-    prompt('It is a Tie!');
+    prompt('Its a  TIE');
   }
 
+  prompt(`You: ${myPoints} | Opponent: ${computerPoints}`);
 
-  prompt('Would you like to play again? (Y/N)');
-  let playAgain = readline.question().toUpperCase();
+  if (myPoints === 3  || computerPoints === 3) {
+    prompt(matchWinner(myPoints, computerPoints));
+    prompt('Would you like to play again? (Y/N)');
+    let again = readline.question().toUpperCase();
+    while (again[0].toUpperCase() !== 'Y' && again[0].toUpperCase() !== 'N') {
+      console.log(again[0].toUpperCase());
+      prompt(`${again} is an invalid input. Please input Y or N`);
+      again = readline.question().toUpperCase();
+    }
 
-  while (playAgain[0] !== 'Y' && playAgain[0] !== 'N') {
-    prompt('Choice invalid. Please input either Y or N:');
-    playAgain = readline.question().toUpperCase();
-  }
-
-  if (playAgain[0].toUpperCase() === 'N') {
-    break;
+    if (again[0].toUpperCase() === 'Y') {
+      myPoints = 0;
+      computerPoints = 0;
+    } else {
+      break;
+    }
   }
 }
 
 console.log('_______________________________________');
-prompt(`Your score: ${myPoints}`);
-prompt(`Computer score ${computerPoints}`);
-prompt(matchWinner(myPoints, computerPoints));
 prompt('Thank you for playing!');
